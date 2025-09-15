d2f18721137a6390866c2243fb1e2df0d4bae746
paul2234 <88509230+paul2234@users.noreply.github.com>
2025-09-15 06:34:20 -0700

    new targets

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ const PORT = 3000;
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get('/', async (req,res) => {
+app.get('/', (req,res) => {
+	res.render('index');
+})
 
-	// instantiate events
-	const events = new Events('fast_finance');
+// app.get('/', async (req,res) => {
+
+// 	// instantiate events
+// 	const events = new Events('fast_finance');
 	
-	// instantiate data and error first
-	let count;
-	let error;
+// 	// instantiate data and error first
+// 	let count;
+// 	let error;
 
-	// load events
-	async function loadEvents() {
-		try {
-			await events.load();
-			count = events.length;	
-		} catch (e) {
-			error = e;
-		}
-	}
+// 	// load events
+// 	async function loadEvents() {
+// 		try {
+// 			await events.load();
+// 			count = events.length;	
+// 		} catch (e) {
+// 			error = e;
+// 		}
+// 	}
 
-	await loadEvents();
+// 	await loadEvents();
 	
-	// render the index template. pass data and error.
-	res.render('index',{count,error});
-});
+// 	// render the index template. pass data and error.
+// 	res.render('index',{count,error});
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
